0aa6854e39c5430504e897d1bf6c2328fe8a671a
Léonard <leonard@bigsool.com>
2021-07-16 10:01:42 +0200

    Cleanup menubar pipes

--- src/app/menubar/menubar.component.ts
+++ src/app/menubar/menubar.component.ts
@@ -4,7 +4,7 @@ import { Clipboard } from '@angular/cdk/clipboard';
 import { ModalService } from '../services/modal.service';
 import { TruncatePipe } from '../pipes/truncate.pipe';
 import { InfoService } from '../services/info.service';
-import { Subscription } from 'rxjs';
+import { debounceTime, defer, Subscription, switchMap, tap } from 'rxjs';
 import { MdbDropdownDirective } from 'mdb-angular-ui-kit';
 
 @Component({
@@ -22,26 +22,38 @@ export class MenubarComponent implements OnInit, OnDestroy {
     private truncate: TruncatePipe,
   ) { }
 
-  private heightChanged: Subscription;
-  private connected: Subscription;
+  private processes: Subscription;
   @ViewChild('dropdown') dropdown: MdbDropdownDirective;
 
   walletText = 'Connect Wallet';
 
   async ngOnInit() {
-    this.heightChanged = this.terrajs.heightChanged.subscribe(async () => {
-      await this.info.refreshBalance({ ust: true, spec: true });
-    });
-    this.connected = this.terrajs.connected.subscribe(connected => {
-      if (connected) {
-        this.walletText = this.getWalletText();
-      } else {
-        this.walletText = 'Connect Wallet';
-      }
-    });
+    // NOTE : Create a composite subscription, we will compose everything into it and unsub everything once on destroy.
+    this.processes = new Subscription();
+    this.processes.add(
+      this.terrajs.heightChanged.pipe(
+        tap(async () => {
+          await this.info.refreshBalance({ ust: true, spec: true });
+        })
+      ).subscribe()
+    );
 
-    // delay to wait for extension to load
-    setTimeout(() => this.initWallet(), 1000);
+    this.processes.add(
+      this.terrajs.connected.pipe(
+        tap((connected) => {
+          if (connected) {
+            this.walletText = this.getWalletText();
+          } else {
+            this.walletText = 'Connect Wallet';
+          }
+        }),
+        // NOTE : SwitchMap means "Subscribe, in a subscribe, we are passing control to "initWallet".
+        // Observables and promises are fully interoperable
+        switchMap(() => {
+          return this.initWallet();
+        }),
+      ).subscribe()
+    );
   }
 
   private async initWallet() {
@@ -54,8 +66,7 @@ export class MenubarComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.heightChanged.unsubscribe();
-    this.connected.unsubscribe();
+    this.processes.unsubscribe();
   }
 
   private getWalletText() {
