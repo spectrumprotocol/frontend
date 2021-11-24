3c090927684d81bbbe9dde2497dae1c650206f5a
SpectrumRhealward <84844988+SpectrumRhealward@users.noreply.github.com>
2021-11-24 16:55:28 +0700

    Total Staked to not show other token, and disable button when loading, add Claim button to Manage Rewards,

--- src/app/pages/vault/unstake-all/unstake-all.component.ts
+++ src/app/pages/vault/unstake-all/unstake-all.component.ts
@@ -21,51 +21,12 @@ export class UnstakeAllComponent {
     protected $gaService: GoogleAnalyticsService,
   ) { }
 
-  getUnstakeAllMsg(): MsgExecuteContract[] {
-    const rewardInfosKeys = Object.keys(this.info.rewardInfos);
-    const rewardInfosKeysThatHavePendingRewards: string[] = [];
-    for (const key of rewardInfosKeys) {
-      if (+this.info.rewardInfos[key].pending_farm_reward > 0 || +this.info.rewardInfos[key].pending_spec_reward > 0) {
-        rewardInfosKeysThatHavePendingRewards.push(key);
-      }
-    }
-    const farmNameListThatHavePendingRewards: Set<string> = new Set();
-    for (const key of rewardInfosKeysThatHavePendingRewards) {
-      farmNameListThatHavePendingRewards.add(this.info.poolInfos[key].farm);
-    }
-    const msgExecuteContractList: MsgExecuteContract[] = [];
-    for (const farmName of farmNameListThatHavePendingRewards) {
-      const findFarm = this.info.farmInfos.find(f => f.farm === farmName);
-      msgExecuteContractList.push(new MsgExecuteContract(
-        this.terrajs.address,
-        findFarm.farmContract,
-        {
-          withdraw: {}
-        }
-      ));
-    }
-    const mintMsg = new MsgExecuteContract(
-      this.terrajs.address,
-      this.terrajs.settings.gov,
-      {
-        mint: {}
-      }
-    );
-    return [mintMsg, ...msgExecuteContractList];
-  }
-
-  async unstakeAll() {
-    this.$gaService.event('CLICK_UNSTAKE_ALL_REWARDS');
-    if (!this.info.portfolio?.total_reward_ust) {
-      return;
-    }
-    await this.terrajs.post(this.getUnstakeAllMsg());
+  disableManageRewards(){
+    return !this.info.portfolio?.total_reward_ust;
   }
 
   async manageRewards() {
-    // TODO:: remove
-
-    if (!this.info.portfolio?.total_reward_ust) {
+    if (this.disableManageRewards()) {
       return;
     }
 
