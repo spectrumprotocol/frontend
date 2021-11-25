8ea2de29a40e4088fddcd628732df7bdabd972eb
hikari042 <spectrum.hikari@gmail.com>
2021-11-25 17:47:43 +0700

    add withdraw to UST flag

--- webpack.config.js
+++ webpack.config.js
@@ -6,21 +6,6 @@ module.exports = {
     new webpack.ProvidePlugin({
       process: 'process/browser',
     })
-  ],
-
-  devServer: {
-    proxy: {
-      '/lcd': {
-         target: {
-            host: "3.34.120.243",
-            protocol: 'http:',
-            port: 1317
-         },
-         pathRewrite: {
-            '^/lcd': ''
-         }
-      }
-   }
-  }
+  ]
   //...
 }
