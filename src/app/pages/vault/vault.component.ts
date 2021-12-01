7292c40571d77cf014c20f532f43a5730840ee23
pui <thepuihs22@gmail.com>
2021-12-01 15:32:22 +0700

    fix design for manage reward on mobile screen and vault asset card

--- src/app/pages/vault/vault.component.ts
+++ src/app/pages/vault/vault.component.ts
@@ -100,8 +100,10 @@ export class VaultComponent implements OnInit, OnDestroy {
   triggerWidth() {
     this.innerWidth = window.innerWidth;
     console.log(this.innerWidth);
-    if(Number(this.innerWidth) <= Number(1024)) {
+    if(Number(this.innerWidth) >= 765 && Number(this.innerWidth) <= Number(1024)) {
       this.isGrid = false;
+    } else {
+      this.isGrid = true;
     }
   }
 
