e0c876a603747d90a20ee3639d0208ce476ddcb3
SpectrumRhealward <84844988+SpectrumRhealward@users.noreply.github.com>
2021-12-21 16:25:56 +0700

    wip zap to ust

--- src/app/services/api/terraswap-router.service.ts
+++ src/app/services/api/terraswap-router.service.ts
@@ -1,31 +1,28 @@
 import { Injectable } from '@angular/core';
 import { WasmService } from './wasm.service';
-import {ExecuteOptions} from '../terrajs.service';
-import {QueryMsg} from './terraswap_pair/query_msg';
-import {PairInfo} from './terraswap_pair/pair_info';
-import {PoolResponse} from './terraswap_pair/pool_response';
-import {SimulationResponse} from './terraswap_pair/simulation_response';
-import {ReverseSimulationResponse} from './terraswap_pair/reverse_simulation_response';
-import {HandleMsg} from './terraswap_pair/handle_msg';
+import {ExecuteOptions, TerrajsService} from '../terrajs.service';
+import {QueryMsg} from './terraswap_router/query_msg';
+import {ConfigResponse} from './terraswap_router/config_response';
+import {ExecuteMsg} from './terraswap_router/execute_msg';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class TerraSwapService {
+export class TerraSwapRouterService {
 
   constructor(
     private wasm: WasmService,
+    private terrajs: TerrajsService
   ) { }
 
-  query(contract: string, msg: Extract<QueryMsg, { pair: unknown }>): Promise<PairInfo>;
-  query(contract: string, msg: Extract<QueryMsg, { pool: unknown }>): Promise<PoolResponse>;
-  query(contract: string, msg: Extract<QueryMsg, { simulation: unknown }>): Promise<SimulationResponse>;
-  query(contract: string, msg: Extract<QueryMsg, { reverse_simulation: unknown }>): Promise<ReverseSimulationResponse>;
-  query(contract: string, msg: QueryMsg): Promise<any> {
-    return this.wasm.query(contract, msg);
+  query(msg: Extract<QueryMsg, { config: unknown }>): Promise<ConfigResponse>;
+  query(msg: Extract<QueryMsg, { simulate_swap_operations: unknown }>): Promise<any>;
+  query(msg: QueryMsg): Promise<any> {
+    return this.wasm.query(this.terrajs.settings.terraSwapRouter, msg);
   }
 
-  handle(contract: string, msg: HandleMsg, opts?: ExecuteOptions) {
-    return this.wasm.execute(contract, msg, opts);
+  handle(msg: ExecuteMsg, opts?: ExecuteOptions) {
+    return this.wasm.execute(this.terrajs.settings.terraSwapRouter, msg, opts);
   }
 }
