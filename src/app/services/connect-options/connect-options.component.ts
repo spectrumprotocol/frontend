f3d6514e6b2048bc47592c6aa21311a3a451a98e
SpectrumRhealward <84844988+SpectrumRhealward@users.noreply.github.com>
2022-02-15 23:57:42 +0700

    wallet extension install links

--- src/app/services/connect-options/connect-options.component.ts
+++ src/app/services/connect-options/connect-options.component.ts
@@ -9,6 +9,26 @@ import {MdbModalRef} from 'mdb-angular-ui-kit/modal';
 export class ConnectOptionsComponent {
   types: string[];
   walletExtensions: any;
+  walletExtensionsForInstall = [
+    {
+      name: 'Terra Station Wallet',
+      identifier: 'station',
+      icon: 'https://assets.terra.money/icon/station-extension/icon.png',
+      installUrl: 'https://chrome.google.com/webstore/detail/terra-station-wallet/aiifbnbfobpmeekipheeijimdpnlpgpp'
+    },
+    {
+      name: 'Leap Wallet',
+      identifier: 'leap-wallet',
+      icon: 'https://leapwallet.io/icon.png',
+      installUrl: 'https://chrome.google.com/webstore/detail/leap-wallet/aijcbedoijmgnlmjeegjaglmepbmpkpi'
+    },
+    {
+      name: 'XDEFI Wallet',
+      identifier: 'xdefi-wallet',
+      icon: 'https://xdefi-prod-common-ui.s3.eu-west-1.amazonaws.com/logo.svg',
+      installUrl: 'https://chrome.google.com/webstore/detail/xdefi-wallet/hmeobnfnfcmdkdcmlblgagmfpfboieaf'
+    }
+  ];
 
   constructor(
     private modalRef: MdbModalRef<ConnectOptionsComponent>
@@ -19,6 +39,10 @@ export class ConnectOptionsComponent {
 
   connect(type: string, identifier: string) {
     this.modalRef.close({type, identifier});
+  }
+
+  findWalletExtensionIdentifier(identifier: string){
+    return !!this.walletExtensions.find(w => w.identifier === identifier);
   }
 
 }
