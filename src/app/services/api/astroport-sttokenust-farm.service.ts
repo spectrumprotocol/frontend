dba069633f87aec1ee9ea56a95d1d3916dd57fb3
SpectrumRhealward <84844988+SpectrumRhealward@users.noreply.github.com>
2022-04-09 18:01:53 +0700

    st schema, api, farm

---
+++ src/app/services/api/astroport-sttokenust-farm.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from '@angular/core';
+import {ExecuteOptions, TerrajsService} from '../terrajs.service';
+import {ConfigInfo} from './astroport_sttoken_ust_farm/config_info';
+import {ExecuteMsg} from './astroport_sttoken_ust_farm/execute_msg';
+import {PoolsResponse} from './astroport_sttoken_ust_farm/pools_response';
+import {QueryMsg} from './astroport_sttoken_ust_farm/query_msg';
+import {RewardInfoResponse} from './astroport_sttoken_ust_farm/reward_info_response';
+import {StateInfo} from './astroport_sttoken_ust_farm/state_info';
+import {WasmService} from './wasm.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AstroportSttokenUstFarmService {
+
+  constructor(
+    private terrajs: TerrajsService,
+    private wasm: WasmService,
+  ) {
+  }
+
+  query(farmAddress: string, msg: Extract<QueryMsg, { config: unknown }>): Promise<ConfigInfo>;
+  query(farmAddress: string, msg: Extract<QueryMsg, { pools: unknown }>): Promise<PoolsResponse>;
+  query(farmAddress: string, msg: Extract<QueryMsg, { reward_info: unknown }>): Promise<RewardInfoResponse>;
+  query(farmAddress: string, msg: Extract<QueryMsg, { state: unknown }>): Promise<StateInfo>;
+  query(farmAddress: string, msg: QueryMsg): Promise<any> {
+    return this.wasm.query(farmAddress, msg);
+  }
+
+  handle(farmAddress: string, msg: ExecuteMsg, opts?: ExecuteOptions) {
+    return this.wasm.execute(farmAddress, msg, opts);
+  }
+}
