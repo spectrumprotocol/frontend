cb9a2afd779d5c488dc9f9ffa300be295199cc29
SpectrumRhealward <84844988+SpectrumRhealward@users.noreply.github.com>
2022-04-11 15:04:45 +0700

    balance_calc to support stluna-ldo

--- src/app/pipes/lp-balance.pipe.ts
+++ src/app/pipes/lp-balance.pipe.ts
@@ -1,7 +1,5 @@
 import {Pipe, PipeTransform} from '@angular/core';
-import {BalancePipe} from './balance.pipe';
 import {InfoService} from '../services/info.service';
-import {TerrajsService} from '../services/terrajs.service';
 import {lp_balance_transform} from '../services/calc/balance_calc';
 
 @Pipe({
@@ -9,13 +7,10 @@ import {lp_balance_transform} from '../services/calc/balance_calc';
 })
 export class LpBalancePipe implements PipeTransform {
 
-  constructor(
-    private balancePipe: BalancePipe,
-    private terrajs: TerrajsService
-  ) {
+  constructor() {
   }
 
   transform(lp: any, info: InfoService, key: string): string {
-    return lp_balance_transform(lp, info.poolResponses, key);
+    return lp_balance_transform(lp, info, key);
   }
 }
--- src/app/services/calc/balance_calc.ts
+++ src/app/services/calc/balance_calc.ts
@@ -1,7 +1,9 @@
 import BigNumber from 'bignumber.js';
 import {Denom} from '../../consts/denom';
-import {div} from '../../libs/math';
+import {div, times} from '../../libs/math';
 import {PoolResponse} from '../api/terraswap_pair/pool_response';
+import {getStablePrice} from '../../libs/stable';
+import {InfoService} from '../info.service';
 
 export const balance_transform = (value: any, poolResponse: PoolResponse, poolResponseB?: PoolResponse) => {
   if (typeof value !== 'string' || !poolResponse) {
@@ -33,11 +35,11 @@ export const balance_transform = (value: any, poolResponse: PoolResponse, poolRe
   }
 };
 
-export const lp_balance_transform = (lp: any, poolResponses: Record<string, PoolResponse>, key: string) => {
-  if ((typeof lp !== 'string' && typeof lp !== 'number') || !poolResponses[key]) {
+export const lp_balance_transform = (lp: any, info: InfoService, key: string) => {
+  if ((typeof lp !== 'string' && typeof lp !== 'number') || !info.poolResponses[key]) {
     return undefined;
   }
-  const poolResponse = poolResponses[key];
+  const poolResponse = info.poolResponses[key];
 
   if (poolResponse.assets[0].info.native_token?.['denom'] === Denom.USD) {
     return new BigNumber(lp)
@@ -51,31 +53,84 @@ export const lp_balance_transform = (lp: any, poolResponses: Record<string, Pool
       .div(poolResponse.total_share)
       .times(2)
       .toString();
+  } else if (key === `Astroport|${info.terrajs.settings.stlunaToken}|${info.terrajs.settings.ldoToken}`) {
+    const stlunaLdoPoolResponses = poolResponse;
+
+    const stlunaLunaPoolResponses = info.poolResponses[`Astroport|${info.terrajs.settings.stlunaToken}|${Denom.LUNA}`];
+    const [ulunaAsset, stluna] = stlunaLunaPoolResponses.assets[0].info.native_token?.['denom'] === Denom.LUNA
+      ? [stlunaLunaPoolResponses.assets[0], stlunaLunaPoolResponses.assets[1]]
+      : [stlunaLunaPoolResponses.assets[1], stlunaLunaPoolResponses.assets[0]];
+    if (!ulunaAsset) {
+      return;
+    }
+    const ulunaPrice = balance_transform('1', info.poolResponses[`Astroport|${Denom.LUNA}|${Denom.USD}`]);
+    const stlunaPriceInLuna = getStablePrice(+stluna.amount, +ulunaAsset.amount);
+    const stlunaPriceInUST = times(ulunaPrice, stlunaPriceInLuna);
+    const stlunaAsset = stlunaLdoPoolResponses.assets.find(a => a.info.token?.['contract_addr'] === info.terrajs.settings.stlunaToken);
+    if (!stlunaAsset) {
+      return;
+    }
+    return new BigNumber(lp)
+      .times(stlunaAsset.amount)
+      .div(stlunaLdoPoolResponses.total_share)
+      .times(stlunaPriceInUST)
+      .times(2)
+      .toString();
   } else {
     const dex = key.split('|')[0];
     const asset0Token: string = poolResponse.assets[0].info.token
       ? poolResponse.assets[0].info.token?.['contract_addr']
       : poolResponse.assets[0].info.native_token?.['denom'];
-    const token1Price = balance_transform('1', poolResponses[`${dex}|${asset0Token}|${Denom.USD}`]);
-    if (token1Price) {
-      return new BigNumber(lp)
-        .times(poolResponse.assets[0].amount)
-        .div(poolResponse.total_share)
-        .times(token1Price)
-        .times(2)
-        .toString();
+    const token0Price = balance_transform('1', info.poolResponses[`${dex}|${asset0Token}|${Denom.USD}`]);
+    if (token0Price) {
+      if (info.pairInfos[key].pair_type?.['stable']) {
+        const asset1Price = getStablePrice(+poolResponse.assets[1].amount, +poolResponse.assets[0].amount);
+        const asset1Swap = new BigNumber(lp)
+          .times(poolResponse.assets[1].amount)
+          .div(poolResponse.total_share)
+          .times(asset1Price)
+          .integerValue(BigNumber.ROUND_DOWN);
+        return new BigNumber(lp)
+          .times(poolResponse.assets[0].amount)
+          .div(poolResponse.total_share)
+          .plus(asset1Swap)
+          .times(token0Price)
+          .toString();
+      } else {
+        return new BigNumber(lp)
+          .times(poolResponse.assets[0].amount)
+          .div(poolResponse.total_share)
+          .times(token0Price)
+          .times(2)
+          .toString();
+      }
     }
     const asset1Token: string = poolResponse.assets[1].info.token
       ? poolResponse.assets[1].info.token?.['contract_addr']
       : poolResponse.assets[1].info.native_token?.['denom'];
-    const token2Price = balance_transform('1', poolResponses[`${dex}|${asset1Token}|${Denom.USD}`]);
-    if (token2Price) {
-      return new BigNumber(lp)
-        .times(poolResponse.assets[1].amount)
-        .div(poolResponse.total_share)
-        .times(token2Price)
-        .times(2)
-        .toString();
+    const token1Price = balance_transform('1', info.poolResponses[`${dex}|${asset1Token}|${Denom.USD}`]);
+    if (token1Price) {
+      if (info.pairInfos[key].pair_type?.['stable']) {
+        const asset0Price = getStablePrice(+poolResponse.assets[0].amount, +poolResponse.assets[1].amount);
+        const asset0Swap = new BigNumber(lp)
+          .times(poolResponse.assets[0].amount)
+          .div(poolResponse.total_share)
+          .times(asset0Price)
+          .integerValue(BigNumber.ROUND_DOWN);
+        return new BigNumber(lp)
+          .times(poolResponse.assets[1].amount)
+          .div(poolResponse.total_share)
+          .plus(asset0Swap)
+          .times(token1Price)
+          .toString();
+      } else {
+        return new BigNumber(lp)
+          .times(poolResponse.assets[1].amount)
+          .div(poolResponse.total_share)
+          .times(token1Price)
+          .times(2)
+          .toString();
+      }
     }
     return null;
   }
